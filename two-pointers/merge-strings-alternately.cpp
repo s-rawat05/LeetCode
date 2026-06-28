e5944a01e313f9ccd9cce97455747564747e6e0e
SAHIL RAWAT <rawatsahil7070@gmail.com>
2026-06-28 19:46:43 +0530

    LC-merge-strings-alternately [Runtime: N/A; Memory: N/A]

---
+++ two-pointers/merge-strings-alternately.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        string ans ="";
+        int n=max(word1.length(),word2.length());//thsis will find the max length of the string 
+        for(int i=0;i<n;i++){
+            if(i<word1.length()){
+                ans+=word1[i];
+            }
+
+            if(i<word2.length()){
+                ans+=word2[i];
+            }
+        }
+        return ans;
+    }
+};
