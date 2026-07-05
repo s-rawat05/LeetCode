c06a0cfbcecc329ce636be667a8164c5c3a282cc
SAHIL RAWAT <rawatsahil7070@gmail.com>
2026-07-05 14:11:32 +0530

    LC-sign-of-the-product-of-an-array [Runtime: N/A; Memory: N/A]

--- math/sign-of-the-product-of-an-array.cpp
+++ math/sign-of-the-product-of-an-array.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     int arraySign(vector<int>& nums) {
-        int pro=1;
-        for(int i=0;i<nums.size();i++){
-            pro*=nums[i];
+        int sign = 1;
+
+        for (int num : nums) {
+            if (num == 0)
+                return 0;
+
+            if (num < 0)
+                sign = -sign;
         }
-        if(pro>0){
-            return 1;
-        }else if(pro==0){
-            return 0;
-        }else{
-            return -1;
-        }
+
+        return sign;
     }
 };
